1a07e152a12c5a0f882a639894ba7d2eb44b201f
raffzhm <rafiazhim.ra@gmail.com>
2023-09-29 12:55:52 +0700

    modified index and js

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
-// Create a map
-const map = L.map('map').setView([-6.906227014340985, 107.66378289976814], 12);
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
-// Add a Tile Layer (use OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+    fetch("gjsonya\fullmap.geojson") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Point") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
 
-// Function to add GeoJSON data to the map and table
-function addGeoJSONToMapAndTable(geoJSONUrl, map, table) {
-    $.getJSON(geoJSONUrl, function (data) {
-        // Add GeoJSON data as a layer to the map
-        L.geoJSON(data).addTo(map);
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
 
-        // Populate the table with GeoJSON data
-        let rowNum = 1; // Initialize a row number counter
+    fetch("https://raw.githubusercontent.com/harisriyoni/gis/main/geojson.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Polygon") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
 
-        data.features.forEach(feature => {
-            const row = table.insertRow();
-            const numCell = row.insertCell(0); // Add a cell for the row number
-            const nameCell = row.insertCell(1);
-            const desaCell = row.insertCell(2);
-            const coordCell = row.insertCell(3); // Cell for coordinates
-            const typeCell = row.insertCell(4); // Add a cell for the type
-            numCell.innerHTML = rowNum; // Populate the row number
-            nameCell.innerHTML = feature.properties.name;
-            desaCell.innerHTML = feature.properties.desa;
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
 
-            // Access the GeoJSON coordinates
-            const coordinates = feature.geometry.coordinates;
-            let coordinateString = "";
-
-            // Check the feature type
-            if (feature.geometry.type === "Point") {
-                const lat = coordinates[1];
-                const long = coordinates[0];
-                coordinateString = `${lat}, ${long}`;
-            } else if (feature.geometry.type === "LineString" || feature.geometry.type === "Polygon") {
-                // Process LineString or Polygon coordinates (as in the previous code)
-                coordinates.forEach(coordinate => {
-                    const lat = coordinate[1];
-                    const long = coordinate[0];
-                    coordinateString += `${lat}, ${long}<br>`;
-                });
-            }
-
-            coordCell.innerHTML = coordinateString; // Populate the coordinate cell
-            typeCell.innerHTML = feature.geometry.type;
-            rowNum++;
-        });
-    });
-}
-
-// Call the function for LineString GeoJSON using the raw URL
-addGeoJSONToMapAndTable('https://raw.githubusercontent.com/raffzhm/geogis1214005/main/gjsonya/fullmap.geojson', map, document.querySelector('table'));
-
-// Call the function for Polygon GeoJSON
-addGeoJSONToMapAndTable('https://raw.githubusercontent.com/raffzhm/geogis1214005/main/gjsonya/fullmap.geojson', map, document.querySelector('table'));
-
-// Call the function for Point GeoJSON
-addGeoJSONToMapAndTable('https://raw.githubusercontent.com/raffzhm/geogis1214005/main/gjsonya/fullmap.geojson', map, document.querySelector('table'));
+    fetch("https://raw.githubusercontent.com/harisriyoni/gis/main/geojson.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "LineString") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
