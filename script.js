555435a74bc77a03d12316587fe13f3a694f8b82
raffzhm <rafiazhim.ra@gmail.com>
2023-09-29 13:04:00 +0700

    modif js and json

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
-    fetch("https://raw.githubusercontent.com/raffzhm/geogis1214005/main/gjsonya/fullmap.geojson") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("./gjsonya/fullmap1.json") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
